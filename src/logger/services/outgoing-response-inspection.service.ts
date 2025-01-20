773bc81c9ea0c8831e5db416b90fac1c962fe57b
ercilli <eric.tarelli@gmail.com>
2025-01-20 09:24:52 -0300

    feat: add OutgoingResponseInspectionService for extracting response data for logging

---
+++ src/logger/services/outgoing-response-inspection.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InterceptorResponseLoggingModel } from '../models/interceptor-response-logging.model';
+
+@Injectable()
+export class OutgoingResponseInspectionService {
+  extractResponseData(
+    status: number,
+    headers: Record<string, any>,
+    body: any,
+    duration: number,
+  ): InterceptorResponseLoggingModel {
+    const model = new InterceptorResponseLoggingModel();
+    model.outgoingHttpResponseStatusCode = status.toString();
+    model.outgoingHttpResponseHeaders = JSON.stringify(headers || {});
+    model.outgoingHttpResponseBody = JSON.stringify(body || {});
+    return model;
+  }
+}
