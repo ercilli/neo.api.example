3a597ce4ffc1ca6da033558a89961659a0a65719
ercilli <eric.tarelli@gmail.com>
2025-01-20 09:24:07 -0300

    feat: add main.ts to bootstrap the application with CORS and trace context

---
+++ src/api/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { TraceContextService } from '../tracer/services/trace-context.service';
+import { Request, Response, NextFunction } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Configurar CORS si es necesario
+  app.enableCors();
+
+  // Configurar contexto asíncrono al inicio de cada solicitud
+  const traceContextService = app.get(TraceContextService);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    traceContextService.run(() => next());
+  });
+
+  // Iniciar la aplicación
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  // Log inicial
+  const logger = new Logger('Bootstrap');
+  logger.log(`Application is running on: http://localhost:${port}`);
+}
+
+bootstrap();
