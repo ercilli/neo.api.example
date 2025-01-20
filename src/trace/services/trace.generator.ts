f674c1511e57297381a476a1ab3b3b5824c95d39
ercilli <eric.tarelli@gmail.com>
2025-01-20 09:25:10 -0300

    feat: remove TraceGenerator as part of refactoring

--- src/trace/services/trace.generator.ts
+++
@@ -1,7 +0,0 @@
-import { randomBytes } from 'crypto';
-
-export class TraceGenerator {
-  static generateTraceId(): string {
-    return randomBytes(16).toString('hex'); // Genera 128 bits (16 bytes) en formato hexadecimal
-  }
-}
